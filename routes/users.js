5ee83c373099bfe7c53c82fce6a4b7a79da090e5
Amanda Durfee <amandadurfee@Amandas-MacBook-Pro.local>
2020-03-23 20:41:01 -0400

    got to work with postman echo

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const fetch = require('node-fetch')
+const router = express.Router();
+
+
+const ticketmasterAPI = {
+  url: 'https://postman-echo.com/get?test='
+}
+
+const doTicketmaster = async (value) => {
+  let returnValueRaw = await fetch(ticketmasterAPI.url + value);
+  let returnValue = await returnValueRaw.json();
+  return returnValue;
+}
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // console.log(req.query.title);
+    doTicketmaster(`${req.query.title}`)
+      .then(returnValue => {
+        console.log(returnValue.args.test + ' is the best singer');
+        res.send('request received accurately!');
+      });
 });
 
 module.exports = router;
