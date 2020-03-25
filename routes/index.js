efb840a5648a9fbae00c7a7c5976039ea95e9878
Amanda Durfee <amandadurfee@Amandas-MacBook-Pro.local>
2020-03-24 22:12:36 -0400

    added dropdown to prototype & bg image

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const usa = [ 'AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT',
+  'DE', 'DC', 'FM', 'FL', 'GA', 'GU', 'HI', 'ID', 'IL', 'IN', 'IA',
+  'KS', 'KY', 'LA', 'ME', 'MH', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO',
+  'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'MP', 'OH',
+  'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT',
+  'VT', 'VI', 'VA', 'WA', 'WV', 'WI', 'WY' ];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index',
+      { title: 'Express' ,
+        states: usa});
 });
+
 
 module.exports = router;
