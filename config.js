0f28bc1bb62f611b8dead4c13ee70bc87f0f9a6f
amandadurfee <45155016+amandadurfee@users.noreply.github.com>
2020-03-24 21:57:36 -0400

    deleted config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-const keys = {
-    ticketmasterKey: 'M6fX66zRt3SRldRHZIU17U2vKjmDvhji',
-    ticketmasterAttractionID: 'https://app.ticketmaster.com/discovery/v2/attractions.json?apikey=M6fX66zRt3SRldRHZIU17U2vKjmDvhji',
-    ticketmasterEvents: 'https://app.ticketmaster.com/discovery/v2/events.json?apikey=M6fX66zRt3SRldRHZIU17U2vKjmDvhji'
-};
-
-module.exports = keys;
