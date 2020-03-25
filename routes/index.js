36d6afd0fa8c742ab7c551059592006327b771cf
Amanda Durfee <amandadurfee@Amandas-MacBook-Pro.local>
2020-03-25 09:18:28 -0400

    alphabetized the usa list bc everyone has it incorrect online

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
 // not in order for gods sake thanks stack overflow
-const usa = [ 'AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT',
-  'DE', 'DC', 'FM', 'FL', 'GA', 'GU', 'HI', 'ID', 'IL', 'IN', 'IA',
-  'KS', 'KY', 'LA', 'ME', 'MH', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO',
-  'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'MP', 'OH',
-  'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT',
-  'VT', 'VI', 'VA', 'WA', 'WV', 'WI', 'WY' ];
+const usa = ["AL", "AK", "AR", "AZ", "CA", "CO", "CT", "DC", "DE", "FL", "GA",
+  "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD",
+  "ME", "MI", "MN", "MO", "MS", "MT", "NC", "ND", "NE", "NH",
+  "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "RI", "SC",
+  "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WI", "WV", "WY"];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
