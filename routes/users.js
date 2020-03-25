847efbbcae2889d9eb65bf151ada3b766e7acbd1
Amanda Durfee <amandadurfee@Amandas-MacBook-Pro.local>
2020-03-24 20:50:17 -0400

    got ticketmaster API events saved for pug template

--- routes/users.js
+++ routes/users.js
@@ -1,26 +1,40 @@
 const express = require('express');
-const fetch = require('node-fetch')
+const fetch = require('node-fetch');
 const router = express.Router();
+const config = require('../config');
 
-
-const ticketmasterAPI = {
-  url: 'https://postman-echo.com/get?test='
-}
-
-const doTicketmaster = async (value) => {
-  let returnValueRaw = await fetch(ticketmasterAPI.url + value);
+const doTicketmaster = async (url, value) => {
+  let returnValueRaw = await fetch(url + value);
   let returnValue = await returnValueRaw.json();
   return returnValue;
-}
+};
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  // console.log(req.query.title);
-    doTicketmaster(`${req.query.title}`)
-      .then(returnValue => {
-        console.log(returnValue.args.test + ' is the best singer');
-        res.send('request received accurately!');
-      });
+    console.log(req.query.title);
+    doTicketmaster(config.ticketmasterAttractionID, `&keyword=${req.query.title}`)
+        // get the attraction ID
+      .then(ticketmasterJSON => {
+        return ticketmasterJSON._embedded.attractions[0].id;
+      })
+
+        // get the events for the attraction ID
+        .then (ticketmasterID => {
+            console.log(ticketmasterID);
+            return doTicketmaster(config.ticketmasterEvents,`&attractionId=${ticketmasterID}&${req.query.state}`);
+        })
+
+            .then (events => {
+                console.log();
+                // render events in json object to be parsed in pug file (see index.js)
+                res.render('layout',
+                    {title:'request received accurately!',
+                     events: JSON.stringify(events._embedded.events)});
+            })
+
+      .catch(e => {
+        console.log(e);
+      })
 });
 
 module.exports = router;
