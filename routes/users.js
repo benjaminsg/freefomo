e82f91e896b7af3b1c92344f450f5d53da7a32b9
Benji Spetter-Goldstein <benjisg@bu.edu>
2020-03-27 19:10:19 -0400

    added Amber's changes to users pug and routing

--- routes/users.js
+++ routes/users.js
@@ -2,39 +2,33 @@ const express = require('express');
 const fetch = require('node-fetch');
 const router = express.Router();
 const config = require('../config');
-
 const doTicketmaster = async (url, value) => {
-  let returnValueRaw = await fetch(url + value);
-  let returnValue = await returnValueRaw.json();
-  return returnValue;
+    let returnValueRaw = await fetch(url + value);
+    let returnValue = await returnValueRaw.json();
+    return returnValue;
 };
-
 /* integrates spotify artist & current location w/ ticketmaster API to get events */
 router.get('/', function(req, res, next) {
     console.log(req.query.title);
     doTicketmaster(config.ticketmasterAttractionID, `&keyword=${req.query.title}`)
-        // get the attraction ID
-      .then(ticketmasterJSON => {
-        return ticketmasterJSON._embedded.attractions[0].id;
-      })
-
+        // get the attraction IDa
+        .then(ticketmasterJSON => {
+            return ticketmasterJSON._embedded.attractions[0].id;
+        })
         // get the events for the attraction ID
         .then (ticketmasterID => {
             console.log(ticketmasterID);
             return doTicketmaster(config.ticketmasterEvents,`&attractionId=${ticketmasterID}&${req.query.state}`);
         })
-
-            .then (events => {
-                console.log();
-                // render events in json object to be parsed in pug file (see index.js)
-                res.render('users',
-                    {title:'request received accurately!',
-                     events: JSON.stringify(events._embedded.events)});
-            })
-
-      .catch(e => {
-        console.log(e);
-      })
+        .then (events => {
+            console.log();
+            // render events in json object to be parsed in pug file (see index.js)
+            res.render('users',
+                {title:'request received accurately!',
+                    events: events._embedded.events})
+        })
+        .catch(e => {
+            console.log(e);
+        })
 });
-
 module.exports = router;
