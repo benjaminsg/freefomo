071f00cf32fa6da22b74b4304967216ef6e8cfdf
Benji Spetter-Goldstein <benjisg@bu.edu>
2020-04-27 14:36:32 -0400

    fixed unsuccessful log in flow

--- routes/login.js
+++ routes/login.js
@@ -41,7 +41,7 @@ router.get("/", function(req, res) {
                                             username:user})
                     } else {
                         console.log("passwords do not match");
-                        res.render('login',
+                        res.render('index',
                             {message: "login unsuccessful"})
                     }
 
